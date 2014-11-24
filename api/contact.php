61259cb4d72288c9a3c116c5143b6ff3898b30d8
Lazaros Kazantzis <lk@amdtelecom.net>
2014-11-24 14:04:33 +0200

    Added API contact us script.

---
+++ api/contact.php
@@ -0,0 +1,42 @@
+<?php
+	$name = $this->request->post['name'];
+	$email = $this->request->post['email'];
+	$subject = 'repro.law.auth.gr - '.$this->request->post['subject'];
+	$message = nl2br($this->request->post['message']);
+	$is_opinion_form = $this->request->post['opinion_form'] == 1 ? true : false;
+	
+	if(empty($name) || empty($email) || empty($subject) || empty($message))
+		die('fill_all');
+	else {
+		if(!$is_opinion_form) {
+			// $to      = 'bioithiki@gmail.com';
+			$to      = 'lakazantzis@gmail.com';
+			$message = '<h3>Μήνυμα από "'.$name.'" μέσω της Φόρμας Επικοινωνίας</h3><div>'.$message.'</div>';
+			// To send HTML mail, the Content-type header must be set
+			$headers  = 'MIME-Version: 1.0' . "\r\n";
+			$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+			
+			// Additional headers
+			$headers .= 'From: '.$name.' <'.$email.'>' . "\r\n";
+			$headers .= 'Reply-To: ' . $email . "\r\n";
+			// $headers .= 'X-Mailer: PHP/' . phpversion();
+		}
+		else {
+			// $to      = 'mimmaki@gmail.com';
+			$to      = 'lakazantzis@gmail.com';
+			$message = '<h3>Μήνυμα από "'.$name.'" μέσω της Φόρμας αποστολής σκέψης/ζητήματος</h3><div>'.$message.'</div>';
+			// To send HTML mail, the Content-type header must be set
+			$headers  = 'MIME-Version: 1.0' . "\r\n";
+			$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+			
+			// Additional headers
+			$headers .= 'From: '.$name.' <'.$email.'>' . "\r\n";
+			$headers .= 'Reply-To: ' . $email . "\r\n";
+			// $headers .= 'Cc: kastan@law.auth.gr' . "\r\n";
+			// $headers .= 'X-Mailer: PHP/' . phpversion();
+		}
+		
+		if(mail($to, $subject, $message, $headers))
+			die('success');
+	}
+?>
